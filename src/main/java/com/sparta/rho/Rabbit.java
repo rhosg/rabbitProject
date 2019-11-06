349355cb18482b3a0a4d42c7e1c6f7b613ad5687
Richard Ho <RHo@spartaglobal.com>
2019-11-06 12:10:31 +0000

    hi

---
+++ src/main/java/com/sparta/rho/Rabbit.java
@@ -0,0 +1,4 @@
+package com.sparta.rho;
+
+public class Rabbit {
+}
---
+++ src/main/java/com/sparta/rho/Starter.java
@@ -0,0 +1,13 @@
+package com.sparta.rho;
+
+/**
+ * Hello world!
+ *
+ */
+public class Starter
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
